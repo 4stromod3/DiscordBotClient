c12be09d954a438c0edca464cfcc3c6035ae9bf8
4stromod3 <4stromode@gmail.com>
2024-05-24 16:55:08 +0200

    Update SystemMessages.js

--- AppAssets/SystemMessages.js
+++ AppAssets/SystemMessages.js
@@ -5,31 +5,17 @@ module.exports = [
 		id: '1000000000000000000',
 		type: 0,
 		channel_id: '1000000000000000000', // If you want to use it, must set ChannelId to 1000000000000000000
-		author: UserData['1056491867375673424'], // Custom User Data here
+		author: UserData['1110633451159306240'], // Custom User Data here
 		attachments: [],
 		content: null,
 		embeds: [
 			{
 				type: 'rich',
-				description: `Thanks for using my client! <:Kanna_Heart:882480441075040257>
-I started this as a hobby project and stayed since ya'll loved it. <:TeriSmile:1048682023839088640>
-And I kid you not, I've never had these many users before!? <a:aqua:857071030689202196>
-
-<a:mikupaylak:863287070407786516> If you had fun, please leave a star on the [repo](https://github.com/aiko-chan-ai/DiscordBotClient) <:github:889092230063734795>
-It really motivates me to work on the project! <:elylove:1065888090549407785>`,
+				description: `Bienvenido a discord!`,
 				color: 16750296,
 				timestamp: '2022-12-31T17:00:00+00:00',
-				author: {
-					name: 'Foreword',
-					icon_url: 'https://i.imgur.com/ThXsjjS.png',
-				},
-				thumbnail: {
-					url: 'https://cdn.discordapp.com/avatars/1056491867375673424/93fb88f6b8c0a2a33c437d0fff4c6625.png',
-					width: 128,
-					height: 128,
-				},
 				footer: {
-					text: 'Elysia',
+					text: 'SnapPix',
 				},
 			},
 		], // Custom Embed Data here
